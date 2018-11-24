2ebe270eee6d2a1432dd2ccb10f8e93da5e9cf64
13xforever <ilya.veselov@gmail.com>
2018-11-24 05:31:27 +0500

    disable debug output to console in release mode

--- IrdLibraryClient/Log.cs
+++ IrdLibraryClient/Log.cs
@@ -31,32 +31,37 @@ namespace IrdLibraryClient
             }
         }
 
-        public static void Trace(Exception e) => LogInternal(e, e?.Message, "TRACE", ConsoleColor.DarkGray);
-        public static void Debug(Exception e) => LogInternal(e, e?.Message, "DEBUG", ConsoleColor.Gray);
-        public static void Info(Exception e) => LogInternal(e, e?.Message, "INFO", ConsoleColor.White);
-        public static void Warn(Exception e) => LogInternal(e, e?.Message, "WARN", ConsoleColor.Yellow);
-        public static void Error(Exception e) => LogInternal(e, e?.Message, "ERROR", ConsoleColor.Red);
-        public static void Fatal(Exception e) => LogInternal(e, e?.Message, "FATAL", ConsoleColor.White, ConsoleColor.Red);
+        public static void Trace(Exception e) => LogInternal(e, e?.Message, LogLevel.TRACE, ConsoleColor.DarkGray);
+        public static void Debug(Exception e) => LogInternal(e, e?.Message, LogLevel.DEBUG, ConsoleColor.Gray);
+        public static void Info(Exception e) => LogInternal(e, e?.Message, LogLevel.INFO, ConsoleColor.White);
+        public static void Warn(Exception e) => LogInternal(e, e?.Message, LogLevel.WARN, ConsoleColor.Yellow);
+        public static void Error(Exception e) => LogInternal(e, e?.Message, LogLevel.ERROR, ConsoleColor.Red);
+        public static void Fatal(Exception e) => LogInternal(e, e?.Message, LogLevel.FATAL, ConsoleColor.White, ConsoleColor.Red);
 
-        public static void Trace(string message) => LogInternal(null, message, "TRACE", ConsoleColor.DarkGray);
-        public static void Debug(string message) => LogInternal(null, message, "DEBUG", ConsoleColor.Gray);
-        public static void Info(string message) => LogInternal(null, message, "INFO", ConsoleColor.White);
-        public static void Warn(string message) => LogInternal(null, message, "WARN", ConsoleColor.Yellow);
-        public static void Error(string message) => LogInternal(null, message, "ERROR", ConsoleColor.Red);
-        public static void Fatal(string message) => LogInternal(null, message, "FATAL", ConsoleColor.White, ConsoleColor.Red);
+        public static void Trace(string message) => LogInternal(null, message, LogLevel.TRACE, ConsoleColor.DarkGray);
+        public static void Debug(string message) => LogInternal(null, message, LogLevel.DEBUG, ConsoleColor.Gray);
+        public static void Info(string message) => LogInternal(null, message, LogLevel.INFO, ConsoleColor.White);
+        public static void Warn(string message) => LogInternal(null, message, LogLevel.WARN, ConsoleColor.Yellow);
+        public static void Error(string message) => LogInternal(null, message, LogLevel.ERROR, ConsoleColor.Red);
+        public static void Fatal(string message) => LogInternal(null, message, LogLevel.FATAL, ConsoleColor.White, ConsoleColor.Red);
 
-        public static void Trace(Exception e, string message) => LogInternal(e, message, "TRACE", ConsoleColor.DarkGray);
-        public static void Debug(Exception e, string message) => LogInternal(e, message, "DEBUG", ConsoleColor.Gray);
-        public static void Info(Exception e, string message) => LogInternal(e, message, "INFO", ConsoleColor.White);
-        public static void Warn(Exception e, string message) => LogInternal(e, message, "WARN", ConsoleColor.Yellow);
-        public static void Error(Exception e, string message) => LogInternal(e, message, "ERROR", ConsoleColor.Red);
-        public static void Fatal(Exception e, string message) => LogInternal(e, message, "FATAL", ConsoleColor.White, ConsoleColor.Red);
+        public static void Trace(Exception e, string message) => LogInternal(e, message, LogLevel.TRACE, ConsoleColor.DarkGray);
+        public static void Debug(Exception e, string message) => LogInternal(e, message, LogLevel.DEBUG, ConsoleColor.Gray);
+        public static void Info(Exception e, string message) => LogInternal(e, message, LogLevel.INFO, ConsoleColor.White);
+        public static void Warn(Exception e, string message) => LogInternal(e, message, LogLevel.WARN, ConsoleColor.Yellow);
+        public static void Error(Exception e, string message) => LogInternal(e, message, LogLevel.ERROR, ConsoleColor.Red);
+        public static void Fatal(Exception e, string message) => LogInternal(e, message, LogLevel.FATAL, ConsoleColor.White, ConsoleColor.Red);
 
-        private static void LogInternal(Exception e, string message, string level, ConsoleColor foregroundColor, ConsoleColor? backgroundColor = null)
+        private static void LogInternal(Exception e, string message, LogLevel level, ConsoleColor foregroundColor, ConsoleColor? backgroundColor = null)
         {
             try
             {
-                if (message != null)
+#if DEBUG
+                const LogLevel minLevel = LogLevel.TRACE;
+#else
+                const LogLevel minLevel = LogLevel.INFO;
+#endif
+                if (level >= minLevel && message != null)
                 {
                     Console.ForegroundColor = foregroundColor;
                     if (backgroundColor is ConsoleColor bg)
@@ -74,6 +79,17 @@ namespace IrdLibraryClient
                 }
             }
             catch { }
+        }
+
+        private enum LogLevel
+        {
+            DISABLED,
+            TRACE,
+            DEBUG,
+            INFO,
+            WARN,
+            ERROR,
+            FATAL,
         }
     }
 }
