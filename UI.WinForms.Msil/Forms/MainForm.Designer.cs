0c6530b81deffc2dd961e068d686d27069dc4b69
13xforever <ilya.veselov@gmail.com>
2019-01-19 05:30:49 +0500

    placeholder icon

--- UI.WinForms.Msil/Forms/MainForm.Designer.cs
+++ UI.WinForms.Msil/Forms/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
             // MainForm
@@ -35,8 +36,11 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MinimizeBox = false;
             this.Name = "MainForm";
-            this.Text = "Form1";
+            this.Text = "PS3 Disc Dumper";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
