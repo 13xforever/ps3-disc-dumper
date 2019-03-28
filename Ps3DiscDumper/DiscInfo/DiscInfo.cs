1cd6b963720b6dfa7f168f16b7f45d4664759c03
13xforever <ilya.veselov@gmail.com>
2019-03-28 20:29:28 +0500

    thinking about the needed metadata

---
+++ Ps3DiscDumper/DiscInfo/DiscInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Ps3DiscDumper.DiscInfo
+{
+    using Hashes = Dictionary<string, byte[]>; // hash type - hash value
+
+    public class DiscInfo
+    {
+        public string ProductCode;    // BLUS12345
+        public string DiscVersion;    // VERSION field from PARAM.SFO
+        public byte[] DiscKeyRawData; // IRD
+        public byte[] DiscKey;        // Redump, and what is actually used for decryption
+        public FileInfo DiscImage;
+        public Dictionary<string, FileInfo> Files;
+    }
+
+    public class FileInfo
+    {
+        public long Offset;
+        public long Size;
+        public Hashes Hashes;
+    }
+}
