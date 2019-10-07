3fde0edc7a199c4537f6bb9c08bf746ffd852777
13xforever <ilya.veselov@gmail.com>
2019-10-07 22:27:37 +0500

    publish for win gui project

--- publish.cmd
+++ publish.cmd
@@ -1,2 +1,5 @@
 @echo off
-dotnet publish -r win-x64 -c Release-Core /p:Platform="Any CPU" /p:DefineConstants="NATIVE"
+rmdir /S /Q "UI.WinForms.Msil/bin"
+rmdir /S /Q "UI.WinForms.Msil/obj"
+dotnet publish -r win-x64 -c Release /p:PublishTrimmed=true;PublishSingleFile=true -o distrib/gui/win/ UI.WinForms.Msil/UI.WinForms.Msil.csproj
+::dotnet publish -r win-x64 -c Release-Core /p:Platform="Any CPU" /p:DefineConstants="NATIVE"
