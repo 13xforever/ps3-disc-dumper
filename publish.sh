75a24f5776852633a5484c492f4b0d55e86f917f
13xforever <ilya.veselov@gmail.com>
2020-01-28 18:49:45 +0500

    fix linux publish

--- publish.sh
+++ publish.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 # sudo apt-get install libcurl4-openssl-dev libkrb5-dev zlib1g-dev curl
-env CppCompilerAndLinker="clang-6.0" dotnet publish -r linux-x64 -c Release-Core /p:DefineConstants="NATIVE"
+rm -rf UI.Console/bin
+rm -rf UI.Console/obj
+dotnet publish -r linux-x64 -c Release -o distrib/cli/lin/ UI.Console/UI.Console.csproj /p:PublishTrimmed=true;PublishSingleFile=true
+#env CppCompilerAndLinker="clang-6.0" dotnet publish -r linux-x64 -c Release-Core /p:DefineConstants="NATIVE"
