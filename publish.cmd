a716bbe5b78607e135b99144944d0938fe68ab5c
13xforever <ilya.veselov@gmail.com>
2020-07-03 15:31:51 +0500

    update deps

--- publish.cmd
+++ publish.cmd
@@ -1,5 +1,7 @@
 @echo off
 rmdir /S /Q "UI.WinForms.Msil/bin"
 rmdir /S /Q "UI.WinForms.Msil/obj"
-dotnet publish -r win-x64 -c Release /p:PublishTrimmed=true;PublishSingleFile=true -o distrib/gui/win/ UI.WinForms.Msil/UI.WinForms.Msil.csproj
-::dotnet publish -r win-x64 -c Release-Core /p:Platform="Any CPU" /p:DefineConstants="NATIVE"
+dotnet build -c Release -r win-x64 UI.WinForms.Msil/UI.WinForms.Msil.csproj
+dotnet publish -r win-x64 -c Release -o distrib/gui/win/ UI.WinForms.Msil/UI.WinForms.Msil.csproj /p:PublishTrimmed=true;PublishSingleFile=true
+dotnet build -c Release -r linux-x64 UI.Console/UI.Console.csproj
+dotnet publish -r linux-x64 -c Release -o distrib/cli/lin/ UI.Console/UI.Console.csproj /p:PublishTrimmed=true;PublishSingleFile=true
