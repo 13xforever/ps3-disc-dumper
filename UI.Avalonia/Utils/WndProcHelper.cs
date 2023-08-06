6ae503db253f33f00edc7f6a7e8e6e5b74d4d8fd
13xforever <ilya.veselov@gmail.com>
2023-08-06 13:14:54 +0500

    handle exception in wndproc hook

--- UI.Avalonia/Utils/WndProcHelper.cs
+++ UI.Avalonia/Utils/WndProcHelper.cs
@@ -4,6 +4,7 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using Avalonia.Controls;
+using IrdLibraryClient;
 using TerraFX.Interop.Windows;
 
 namespace UI.Avalonia.Utils;
@@ -16,22 +17,42 @@ public static unsafe class WndProcHelper
     private static delegate* unmanaged<HWND, uint, WPARAM, LPARAM, LRESULT> wrappedWndProc;
     private static OnWndProc userFunc = null!;
 
-    public static void Register(Window window, OnWndProc onWndProc)
+    public static bool Register(Window window, OnWndProc onWndProc)
     {
-        userFunc = onWndProc;
-        var windowImpl = window.PlatformImpl!;
-        //var platformHandle = windowImpl.GetType().GetProperty("Handle", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(windowImpl);
-        var windowHandle = (IntPtr)windowImpl.GetType().GetProperty("Hwnd", BindingFlags.Instance | BindingFlags.NonPublic)!.GetValue(windowImpl)!;
-        var hwnd = (HWND)windowHandle;
-        wrappedWndProc = (delegate* unmanaged<HWND, uint, WPARAM, LPARAM, LRESULT>)Windows.GetWindowLongPtr(hwnd, GWLP.GWLP_WNDPROC);
-        delegate* unmanaged<HWND, uint, WPARAM, LPARAM, LRESULT> wrapperWinProc = &WndProcHook;
-        Windows.SetWindowLongPtr(hwnd, GWLP.GWLP_WNDPROC, (nint)wrapperWinProc);
+        try
+        {
+            userFunc = onWndProc;
+            var windowImpl = window.PlatformImpl!;
+            //var platformHandle = windowImpl.GetType().GetProperty("Handle", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(windowImpl);
+            var windowHandle =
+                (IntPtr)windowImpl.GetType().GetProperty("Hwnd", BindingFlags.Instance | BindingFlags.NonPublic)!
+                    .GetValue(windowImpl)!;
+            var hwnd = (HWND)windowHandle;
+            wrappedWndProc =
+                (delegate* unmanaged<HWND, uint, WPARAM, LPARAM, LRESULT>)Windows.GetWindowLongPtr(hwnd,
+                    GWLP.GWLP_WNDPROC);
+            delegate* unmanaged<HWND, uint, WPARAM, LPARAM, LRESULT> wrapperWinProc = &WndProcHook;
+            Windows.SetWindowLongPtr(hwnd, GWLP.GWLP_WNDPROC, (nint)wrapperWinProc);
+            return true;
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Failed to hook the WndProc event loop");
+            return false;
+        }
     }
 
     [UnmanagedCallersOnly]
     public static LRESULT WndProcHook(HWND hwnd, uint msg, WPARAM wParam, LPARAM lParam)
     {
-        userFunc(hwnd, msg, wParam, lParam);
+        try
+        {
+            userFunc(hwnd, msg, wParam, lParam);
+        }
+        catch (Exception e)
+        {
+            Log.Error(e, "Exception in user-defined event loop hook");
+        }
         return wrappedWndProc(hwnd, msg, wParam, lParam);
     }
 }
