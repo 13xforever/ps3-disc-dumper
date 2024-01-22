8b9743acdd4dc4fc606254e77d88001be8d9d5c9
13xforever <ilya.veselov@gmail.com>
2024-01-22 14:39:38 +0500

    bump version

--- Ps3DiscDumper/Dumper.cs
+++ Ps3DiscDumper/Dumper.cs
@@ -27,7 +27,7 @@ namespace Ps3DiscDumper;
 
 public class Dumper: IDisposable
 {
-    public const string Version = "4.0.8";
+    public const string Version = "4.0.9";
 
     static Dumper() => Log.Info("PS3 Disc Dumper v" + Version);
 
