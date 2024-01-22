74daab862e79a937e6ccebad410dd3e5e71c21ff
13xforever <ilya.veselov@gmail.com>
2024-01-22 20:53:58 +0500

    rescan disc if settings were changed before the dump was started

--- Ps3DiscDumper/Dumper.cs
+++ Ps3DiscDumper/Dumper.cs
@@ -27,7 +27,7 @@ namespace Ps3DiscDumper;
 
 public class Dumper: IDisposable
 {
-    public const string Version = "4.0.9";
+    public const string Version = "4.1.0";
 
     static Dumper() => Log.Info("PS3 Disc Dumper v" + Version);
 
--- UI.Avalonia/ViewModels/MainWindowViewModel.cs
+++ UI.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Linq;
+using Avalonia.Threading;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using Ps3DiscDumper;
@@ -12,6 +13,7 @@ public partial class MainWindowViewModel : ObservableObject, IDisposable
 {
     private readonly SettingsViewModel settingsPage = new();
     private readonly MainViewModel mainPage;
+    private Settings oldSettings;
 
     public MainWindowViewModel() => CurrentPage = mainPage = new(settingsPage);
 
@@ -25,16 +27,36 @@ public partial class MainWindowViewModel : ObservableObject, IDisposable
     [ObservableProperty] private string formattedUpdateInfoBody = "";
     [ObservableProperty] private string formattedUpdateInfoUrl = $"{SettingsViewModel.ProjectUrl}/releases/latest";
     [ObservableProperty] private string formattedUpdateInfoVersion = "";
+    
 
     [RelayCommand]
     private void ToggleSettingsPage()
     {
         if (CurrentPage is MainViewModel)
+        {
+            oldSettings = SettingsProvider.Settings;
             CurrentPage = settingsPage;
+        }
         else
         {
             SettingsProvider.Save();
             CurrentPage = mainPage;
+            if (!mainPage.DumperIsReady)
+                return;
+            
+            var newSettings = SettingsProvider.Settings;
+            if (newSettings.DumpNameTemplate != oldSettings.DumpNameTemplate
+                || newSettings.OutputDir != oldSettings.OutputDir
+                || newSettings.IrdDir != oldSettings.IrdDir
+                || newSettings.CopyBdmv != oldSettings.CopyBdmv
+                || newSettings.CopyPs3Update != oldSettings.CopyPs3Update)
+            {
+                Dispatcher.UIThread.Post(() =>
+                {
+                    mainPage.ResetViewModelCommand.Execute(null);
+                    mainPage.ScanDiscsCommand.Execute(null);
+                }, DispatcherPriority.Background);
+            }
         }
     }
 
