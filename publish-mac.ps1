20572f1981a09288af7a777a31fdd805527591e7
Steveice10 <1269164+Steveice10@users.noreply.github.com>
2024-02-11 04:21:44 -0800

    Fix code signing and version on macOS published build.

--- publish-mac.ps1
+++ publish-mac.ps1
@@ -14,17 +14,21 @@ Remove-Item -LiteralPath UI.Avalonia/obj -Recurse -Force -ErrorAction SilentlyCo
 
 Write-Host 'Building macOS binary...' -ForegroundColor Cyan
 dotnet publish -v:q -t:BundleApp -r osx-arm64 -f net8.0 --self-contained -c MacOS -o distrib/gui/mac/ UI.Avalonia/UI.Avalonia.csproj /p:PublishTrimmed=False /p:PublishSingleFile=True
-if (($LASTEXITCODE -eq 0) -and ($IsMacOS -or ($PSVersionTable.Platform -eq 'Unix')))
-{
-    chmod +x distrib/gui/mac/ps3-disc-dumper
-}
 
 Write-Host 'Clearing extra files in distrib...' -ForegroundColor Cyan
 Get-ChildItem -LiteralPath distrib -Include *.pdb,*.config -Recurse | Remove-Item
 
-Write-Host 'Bundling...' -ForegroundColor Cyan
-if (Test-Path -LiteralPath distrib/gui/mac/ps3-disc-dumper)
+if (($LASTEXITCODE -eq 0) -and ($IsMacOS -or ($PSVersionTable.Platform -eq 'Unix')))
 {
+    chmod +x distrib/gui/mac/ps3-disc-dumper
+    # The final app bundle needs to be re-signed as a whole.
+    codesign --deep -fs - 'distrib/gui/mac/PS3 Disc Dumper.app'
+}
+
+Write-Host 'Bundling...' -ForegroundColor Cyan
+if (Test-Path -LiteralPath 'distrib/gui/mac/PS3 Disc Dumper.app')
+{
+    # tar to preserve file permissions.
     tar -C distrib/gui/mac -cvzf distrib/ps3-disc-dumper_macos_NEW.tar.gz 'PS3 Disc Dumper.app'
 }
 
