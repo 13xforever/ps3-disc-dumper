e2795cc160fde846c98b7c2e3ea6c30c7285abd4
13xforever <ilya.veselov@gmail.com>
2024-02-11 15:24:10 +0500

    update publish-mac.ps1 as it's technically working on all platforms now

--- publish-mac.ps1
+++ publish-mac.ps1
@@ -12,12 +12,6 @@ Write-Host 'Clearing bin/obj...' -ForegroundColor Cyan
 Remove-Item -LiteralPath UI.Avalonia/bin -Recurse -Force -ErrorAction SilentlyContinue
 Remove-Item -LiteralPath UI.Avalonia/obj -Recurse -Force -ErrorAction SilentlyContinue
 
-if (-not $IsMacOS)
-{
-    Write-Error "Building macOS binary requires macOS"
-    return
-}
-
 Write-Host 'Building macOS binary...' -ForegroundColor Cyan
 dotnet publish -v:q -t:BundleApp -r osx-arm64 -f net8.0 --self-contained -c MacOS -o distrib/gui/mac/ UI.Avalonia/UI.Avalonia.csproj /p:PublishTrimmed=False /p:PublishSingleFile=True
 if (($LASTEXITCODE -eq 0) -and ($IsMacOS -or ($PSVersionTable.Platform -eq 'Unix')))
@@ -28,7 +22,7 @@ if (($LASTEXITCODE -eq 0) -and ($IsMacOS -or ($PSVersionTable.Platform -eq 'Unix
 Write-Host 'Clearing extra files in distrib...' -ForegroundColor Cyan
 Get-ChildItem -LiteralPath distrib -Include *.pdb,*.config -Recurse | Remove-Item
 
-Write-Host 'Zipping...' -ForegroundColor Cyan
+Write-Host 'Bundling...' -ForegroundColor Cyan
 if (Test-Path -LiteralPath distrib/gui/mac/ps3-disc-dumper)
 {
     tar -C distrib/gui/mac -cvzf distrib/ps3-disc-dumper_macos_NEW.tar.gz 'PS3 Disc Dumper.app'
